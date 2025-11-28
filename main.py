781132bf0306129299ec6e65885dcabbb01168a8
William Lewis Jr <wlewisjr586@proton.me>
2025-11-28 18:42:05 -0500

    Limit to 60fps.

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -20,6 +22,7 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
